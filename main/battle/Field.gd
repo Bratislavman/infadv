56ae297fd2090b7943baa904342c51c68bc0cdb5
Bratislavman <kiro241@gmail.com>
2023-12-31 20:38:37 +0300

    учёба
    попробывал создавать персонажей и ставить их в позицию дочерних нод сцены
    
    опробывал анимации спрайта и ловлю события из анимации в нужном кадре что бы вызвать метод(активировать другую анимацию)

--- main/battle/Field.gd
+++ main/battle/Field.gd
@@ -1,17 +1,29 @@
 extends Node2D
 
+var npc = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 		G.mission.hell()
 		get_tree().call_group('all', 'chc')
 		get_node("bg").texture = preload('res://missons/star-wars/battle/battles/beast/bg.jpg')
+		var rexScene = preload('res://missons/star-wars/battle/characters/rex/Rex.tscn')
+		npc = rexScene.instantiate()
+		var heroPost = get_node("Cell1_3Hero")
+		npc.position = heroPost.position
+		add_child(npc)
+		
+		
 	
 
 
 func hell():
 	print('hell')
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+
+func _input(event):
+	if (npc):
+		if event is InputEventKey and event.pressed:
+			if event.keycode == KEY_U:
+				npc.playAnim('attack')
+				
