548c93f0c43a604a85819134d7b3449bad845e11
Bratislavman <kiro241@gmail.com>
2025-07-10 13:23:21 +0300

    пофиксил чтоб спецэффект от удара был посередине спрайта цели

--- main/battle/command/CommandAttackMelee.gd
+++ main/battle/command/CommandAttackMelee.gd
@@ -6,12 +6,15 @@ var hitClass = preload("res://main/battle/effects/hit/Hit.tscn")
 func _init(caster: Unit, target: Unit):
 	var dmg = func():
 		target.dmg(caster.attributes[Attributes.attrNameDmg].value)
+		
 		var effect = hitClass.instantiate()
 
 		G.battleController.add_child(effect)
 		effect.position = target.position
+		effect.position.y -= target._mouse_hendler_area.size.y/2
 
 		effect.init(self)
+
 		effects.append(effect)
 		
 	super._init(caster, target, "attack", dmg)
