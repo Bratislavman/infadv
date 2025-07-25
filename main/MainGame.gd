872e796641e796eb6d9bf7bb25c296504756066d
Bratislavman <kiro241@gmail.com>
2025-07-25 23:55:12 +0300

    заготовка модалки под инфу персов и спелов

--- main/MainGame.gd
+++ main/MainGame.gd
@@ -1,6 +1,7 @@
 extends Node
 
 var battleController = null
+var modalInfo = null
 var battle = null
 var mainScene = null
 
@@ -9,6 +10,12 @@ func _ready() -> void:
 	battleController = fieldClass.instantiate()
 	battleController.visible = false
 	add_child(battleController)
+
+
+	var modalInfoClass = preload("res://main/battle/modals/info/ModalInfo.tscn")
+	modalInfo = modalInfoClass.instantiate()
+	modalInfo.visible = false
+	add_child(modalInfo)
 
 func _process(_delta):
 	battleController.action()
